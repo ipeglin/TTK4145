2e204b4a71263715fb968a01bd1703a18596e73b
Simen Fritzner <simenfritzner@gmail.com>
2024-02-14 22:09:58 +0100

    fixed up

--- Project/Elevator/elevator/elevator.go
+++ Project/Elevator/elevator/elevator.go
@@ -1,23 +1,15 @@
 package main
 
 func elevator_init() Elevator {
-	return (Elevator) {
-		CurrentFloor = -1
-		CurrentBehaviour = EBIdle
-		Config = {
-			ClearRequestVariant = ClearRequestVariantConfig
-			DoorOpenDurationS = DoorOpenDurationSConfig
-		}
-	}
-}
-
-type Elevator struct {
-	CurrentFloor int
-	Requests [N_FLOORS][N_BUTTONS]int
-	CurrentBehaviour ElevatorBehaviour
-
-	Config struct {
-        ClearRequestVariant ClearRequestVariant
-        DoorOpenDurationS   float64
+    return Elevator{
+        CurrentFloor: -1,
+        CurrentBehaviour: EBIdle,
+        Config: struct {
+            ClearRequestVariant ClearRequestVariant
+            DoorOpenDurationS   float64
+        }{
+            ClearRequestVariant: ClearRequestVariantConfig,
+            DoorOpenDurationS:   DoorOpenDurationSConfig,
+        },
     }
 }
