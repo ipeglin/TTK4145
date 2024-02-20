b8c9f71408e7ac3b4794a7a73b4ae3a664fe12dc
ipeglin <philip@eglin.no>
2024-02-20 16:04:27 +0100

    refactor: separating init and network module. Network module now forwards messages from broadcasts to init instance in ./init/init.go

---
+++ Project/init/init.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"network"
+	"network/nodes"
+)
+
+// fetching process flags
+func getFlags() (int, int, error) {
+	var id, numNodes *int
+	id = flag.Int("id", 0, "ID of the node")
+	numNodes = flag.Int("numNodes", 3, "total number of the nodes")
+	flag.Parse()
+
+	// require id
+	if *id == 0 {
+		return 0, 0, fmt.Errorf("ERROR: Node ID is required")
+	} else if *id > *numNodes {
+		return 0, 0, fmt.Errorf("ERROR: Node ID cannot be greater than number of nodes")
+	}
+
+	return *id, *numNodes, nil
+}
+
+func main() {
+	id, numNodes, err := getFlags()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("Node initialised with:\n")
+	fmt.Printf("  ID:          %d\n", id)
+	fmt.Printf("  #Nodes:      %d\n", numNodes)
+
+	nodeOverviewChannel := make(chan nodes.NetworkNodeRegistry)
+	messageReceiveChannel := make(chan network.Message)
+	messageTransmitterChannel := make(chan network.Message)
+
+	go network.Init(id, nodeOverviewChannel, messageTransmitterChannel, messageReceiveChannel)
+
+	for {
+		select {
+		case reg := <-nodeOverviewChannel:
+			fmt.Printf("Peer update:\n")
+			fmt.Printf("  Peers:    %q\n", reg.Nodes)
+			fmt.Printf("  New:      %q\n", reg.New)
+			fmt.Printf("  Lost:     %q\n", reg.Lost)
+
+		case msg := <-messageReceiveChannel:
+			fmt.Printf("Network module says:%v\n", msg)
+		}
+	}
+}
---
+++ Project/network/network.go
@@ -0,0 +1,65 @@
+package network
+
+import (
+	"fmt"
+	"network/broadcast"
+	"network/local"
+	"network/nodes"
+	"os"
+)
+
+const basePort int = 1337
+const lifelinePort int = basePort + 1
+const transmissionPort int = basePort + 2
+const receiverPort int = basePort + 3
+
+type Message struct {
+	Content    string
+	Iterations int
+}
+
+func Init(nodeId int, nodesChannel chan<- nodes.NetworkNodeRegistry, messageChannel chan<- Message, responseChannel <-chan Message) {
+	fmt.Println("Initialising Network Module...")
+
+	// fetching host IP and PORT
+	nodeIP, err := local.GetIP()
+	if err != nil {
+		fmt.Println("ERROR: Unable to get the IP address")
+		nodeIP = "Disconnected"
+	}
+
+	// set node unique ID
+	nodeUid := fmt.Sprintf("peer-%s-%d", nodeIP, os.Getpid())
+
+	fmt.Printf("Module initialised with:\n")
+	fmt.Printf("  IPv4:     %v\n", nodeIP)
+	fmt.Printf("  PORT:     %d\n", basePort)
+	fmt.Printf("  UID:      %s\n", nodeUid)
+
+	// channel for network node updates
+	nodeRegistryChannel := make(chan nodes.NetworkNodeRegistry)
+	TransmissionEnableChannel := make(chan bool)
+
+	go nodes.Client(lifelinePort, nodeUid, TransmissionEnableChannel)
+	go nodes.Server(lifelinePort, nodeRegistryChannel)
+
+	broadcastTransmissionChannel := make(chan Message)
+	broadcastReceiverChannel := make(chan Message)
+
+	go broadcast.Client(transmissionPort, broadcastTransmissionChannel)
+	go broadcast.Server(receiverPort, broadcastReceiverChannel)
+
+	fmt.Println("Network module starting...")
+	for {
+		select {
+		case reg := <-nodeRegistryChannel:
+			nodesChannel <- reg
+
+		case msg := <-broadcastReceiverChannel:
+			messageChannel <- msg
+
+		case msg := <-responseChannel:
+			broadcastTransmissionChannel <- msg
+		}
+	}
+}
