aba9d9f34c17ece1c74f1dd18428d578cbdf5d44
ipeglin <philip@eglin.no>
2024-03-02 15:08:11 +0100

    refactor: only printing payload

--- Project/init/init.go
+++ Project/init/init.go
@@ -33,7 +33,7 @@ func main() {
 		case reg := <-nodeOverviewChannel:
 			logrus.Info("Known nodes:", reg.Nodes)
 		case msg := <-messageReceiveChannel:
-			fmt.Print("Must do something with the message:", msg)
+			fmt.Print("Must do something with the message:", msg.Payload)
 		case online:= <-onlineStatusChannel:
 			logrus.Warn("Updated online status:", online)
 		}
