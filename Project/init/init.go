b97c8ae4eeb704be1ebb5aa5dfa379e27036a96f
philip@eglin.no <0>
2024-03-06 19:25:47 +0100

    Broadcast state 10 times pr sec

--- Project/init/init.go
+++ Project/init/init.go
@@ -32,7 +32,7 @@ func main() {
 		for {
 			elv, _ := checkpoint.LoadCombinedInput(checkpoint.JSONFile)
 			messageTransmitterChannel <- network.Message{Payload: elv, MessageId: 0}
-			time.Sleep(5 * time.Second)
+			time.Sleep(100 * time.Millisecond)
 		}
 	}()
 
