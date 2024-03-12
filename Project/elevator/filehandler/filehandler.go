7fd1181795745de61d526b8c6a687de0ab933979
ipeglin <philip@eglin.no>
2024-03-12 12:02:11 +0100

    feat: adding filehandler logging

--- Project/elevator/filehandler/filehandler.go
+++ Project/elevator/filehandler/filehandler.go
@@ -4,46 +4,51 @@ import (
 	"fmt"
 	"os"
 	"syscall"
+
+	"github.com/sirupsen/logrus"
 )
 
-func WriteToFile(data []byte, fileName string) error {
-	osFile, err := lockFile(fileName)
+func WriteToFile(data []byte, filename string) error {
+	osFile, err := lockFile(filename)
 	if err != nil {
 		return err
-
 	}
 	defer unlockFile(osFile) // Ensure file is unlocked after reading
 
-	err = os.WriteFile(fileName, data, 0644)
+	err = os.WriteFile(filename, data, 0644)
 	if err != nil {
 		return err
 	}
+	logrus.Debug("Successfully wrote to file: ", filename)
 	return nil
 }
 
-func ReadFromFile(fileName string) ([]byte, error) {
-	osFile, err := lockFile(fileName) // Lock the file for reading
+func ReadFromFile(filename string) ([]byte, error) {
+	osFile, err := lockFile(filename) // Lock the file for reading
 	if err != nil {
 		return nil, err
 	}
 	defer unlockFile(osFile) // Ensure file is unlocked after reading
 
-	data, err := os.ReadFile(fileName)
+	data, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read checkpoint file: %v", err)
 	}
+	logrus.Debug("Successfully read from file: ", filename)
 	return data, nil
 }
 
-func lockFile(filePath string) (*os.File, error) {
-	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR, 0666)
+func lockFile(filename string) (*os.File, error) {
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0666)
 	if err != nil {
 		return nil, err
 	}
+
 	err = syscall.Flock(int(file.Fd()), syscall.LOCK_EX)
 	if err != nil {
 		return nil, err
 	}
+	logrus.Debug("Locked file: ", filename)
 	return file, nil
 }
 
@@ -53,5 +58,6 @@ func unlockFile(file *os.File) error {
 	if err != nil {
 		return err
 	}
+	logrus.Debug("Unlocked file: ", file.Name())
 	return closeErr
 }
